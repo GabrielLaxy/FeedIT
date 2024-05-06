f5dc1a6478c198a9221f6c37f1e3018849f7852f
Gabriel Laxy <gablaxycarreira@gmail.com>
2024-05-05 23:51:19 -0300

    inicio da tela home e da funcao musicBackground

--- screens/home.js
+++ screens/home.js
@@ -1,12 +1,51 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text, Image, ImageBackground, StyleSheet, Dimensions } from 'react-native';
 
-export default function Home(){
-    return(
-        <View>
-            <Text>
-                Home
-            </Text>
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const backgroundImage = require('../assets/home_background.png');
+const dino_lv1 = require('../assets/dino_lv1.png');
+const shadow = require('../assets/sombra.png')
+
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <ImageBackground source={backgroundImage} resizeMode="cover" style={styles.imageBackground}>
+                <Image source={shadow} style={styles.shadow}/>
+                <Image source={dino_lv1} style={styles.dino} />
+            </ImageBackground>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    imageBackground: {
+        flex: 1,
+        width: windowWidth,
+        height: windowHeight,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: 'white',
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    dino:{
+        width: 220,
+        height:220,
+        position: 'absolute',
+        bottom:120,
+    },
+    shadow:{
+        width:441,
+        height:184,
+        position:'absolute',
+        bottom:30
+    }
+});
