33cabf31aee30c2bd4533808e5125b0a587a5387
Gabriel Laxy <gablaxycarreira@gmail.com>
2024-05-13 21:59:59 -0300

    Adição de features na Home

--- screens/home.js
+++ screens/home.js
@@ -1,20 +1,51 @@
-import React from 'react';
-import { View, Text, Image, ImageBackground, StyleSheet, Dimensions } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, Text, Image, ImageBackground, StyleSheet, Dimensions, Animated, TouchableOpacity } from 'react-native';
+import { Entypo } from '@expo/vector-icons'; // Importe o ícone que você deseja usar
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 const backgroundImage = require('../assets/home_background.png');
 const dino_lv1 = require('../assets/dino_lv1.png');
-const shadow = require('../assets/sombra.png')
-
+const shadow = require('../assets/sombra.png');
 
 export default function Home() {
+    const panelYPosition = useRef(new Animated.Value(-windowHeight)).current;
+    const [panelOpen, setPanelOpen] = useState(false);
+
+    const togglePanel = () => {
+        const toValue = panelOpen ? -windowHeight : 0;
+        Animated.timing(panelYPosition, {
+            toValue: toValue,
+            duration: 500,
+            useNativeDriver: false
+        }).start();
+        setPanelOpen(!panelOpen);
+    };
+
+    const panelStyle = {
+        transform: [{ translateY: panelYPosition }],
+        backgroundColor: '#FBFEF4',
+        position: 'absolute',
+        top: 0,
+        width: '100%',
+        height: windowHeight / 3,
+        borderBottomLeftRadius: 40,
+        borderBottomRightRadius: 40,
+    };
+
+
     return (
         <View style={styles.container}>
             <ImageBackground source={backgroundImage} resizeMode="cover" style={styles.imageBackground}>
                 <Image source={shadow} style={styles.shadow}/>
                 <Image source={dino_lv1} style={styles.dino} />
+                <TouchableOpacity onPress={togglePanel} style={styles.iconContainer}>
+                    <Entypo name={panelOpen ? "arrow-with-circle-up" : "arrow-with-circle-down"} size={24} color="black" />
+                </TouchableOpacity>
+                <Animated.View style={panelStyle}>
+                    <Text>Status</Text>
+                </Animated.View>
             </ImageBackground>
         </View>
     );
@@ -31,11 +62,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    text: {
-        color: 'white',
-        fontSize: 24,
-        fontWeight: 'bold',
-    },
     dino:{
         width: 220,
         height:220,
@@ -47,5 +73,12 @@ const styles = StyleSheet.create({
         height:184,
         position:'absolute',
         bottom:30
-    }
+    },
+    iconContainer: {
+        position: 'absolute',
+        top: 20, // Posição do ícone a partir do topo da tela
+        right: 20, // Posição do ícone a partir da direita da tela
+        zIndex: 1, // Garante que o ícone esteja acima do conteúdo da tela
+    },
+    // Adicione estilos adicionais conforme necessário
 });
