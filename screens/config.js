57ad209c6a605208d04c2856c5b5c252307da587
Gabriel Laxy <gablaxycarreira@gmail.com>
2024-05-19 22:11:15 -0300

    alteracoes styles config page

--- screens/config.js
+++ screens/config.js
@@ -1,55 +1,55 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default function Config() {
   return (
     <View style={{ backgroundColor: "#FBFEF4", flex: 1 }}>
-      <Text style={titulo}>
+      <Text style={styles.titulo}>
         {'\n'}Configurações
       </Text>
-      <Text style={linha}>
+      <Text style={styles.linha}>
         -    -    -    -    -    -    -    -    -    -    -    -
         {/* Preciso descobrir como fazer essa linha de um jeito mais limpo, tentei colocar uma borda pontilhada e não deu certo */}
       </Text>
-      <Text style={topicos}>Sons</Text>
-      <Text style={topicos}>Música</Text>
+      <Text style={styles.topicos}>Sons</Text>
+      <Text style={styles.topicos}>Música</Text>
       <TouchableOpacity onPress={() => alert('Perfil clicked')}>
-        <Text style={topicos}>Perfil</Text>
+        <Text style={styles.topicos}>Perfil</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => alert('Suporte clicked')}>
-        <Text style={topicos}>Suporte</Text>
+        <Text style={styles.topicos}>Suporte</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => alert('Sair clicked')}>
-        <Text style={topicos}>Sair</Text>
+        <Text style={styles.topicos}>Sair</Text>
       </TouchableOpacity>
     </View>
   );
 }
-
-const titulo = {
-  backgroundColor: "#FBFEF4",
-  color: "#5C4B4B",
-  fontFamily: "Poppins_700Bold",
-  fontSize: 27,
-  textAlign: 'center',
-};
-
-const linha = {
-  backgroundColor: "#FBFEF4",
-  color: "#5C4B4B",
-  fontFamily: "Poppins_700Bold",
-  fontSize: 20,
-  textAlign: 'center',
-  marginTop: 20,
-  margin:20,
-};
-
-const topicos = {
-  backgroundColor: "#FBFEF4",
-  color: "#5C4B4B",
-  fontFamily: "Poppins_700Bold",
-  fontSize: 20,
-  textAlign: 'left',
-  marginTop: 20,
-  marginLeft: 50,
-};
+const styles = StyleSheet.create({
+  titulo: {
+    backgroundColor: "#FBFEF4",
+    color: "#5C4B4B",
+    fontFamily: "Poppins_700Bold",
+    fontSize: 27,
+    textAlign: 'center',
+  },
+  linha: {
+    backgroundColor: "#FBFEF4",
+    color: "#5C4B4B",
+    fontFamily: "Poppins_700Bold",
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 20,
+    margin:20,
+  },
+  topicos: {
+    backgroundColor: "#FBFEF4",
+    color: "#5C4B4B",
+    fontFamily: "Poppins_700Bold",
+    fontSize: 20,
+    textAlign: 'left',
+    marginTop: 20,
+    marginLeft: 50,
+    textDecorationLine:'underline',
+  },
+});
