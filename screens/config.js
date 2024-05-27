af24e90c4ff29de0a1d666d89505133301dd7c1e
Gabriel Laxy <gablaxycarreira@gmail.com>
2024-05-27 13:13:04 -0300

    tela Configs com todas as funcionalidades ok

--- screens/config.js
+++ screens/config.js
@@ -1,55 +1,119 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Linking, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import DashedLine from 'react-native-dashed-line';
+import Slider from '@react-native-community/slider';
+import { Entypo } from '@expo/vector-icons';
+
+const volume = 0.5; // Definindo um valor inicial para o volume
 
 export default function Config() {
+  const navigation = useNavigation();
+
+  const emailSuporte = () => {
+    Alert.alert(
+      'Contate o suporte',
+      'Você será redirecionado para seu aplicativo de e-mail e contactar o suporte do FeedIt. Deseja continuar?',
+      [
+        {
+          text: 'Não',
+          onPress: () => console.log('Cancelado'),
+          style: 'cancel',
+        },
+        { text: 'Sim', onPress: () => Linking.openURL('mailto:gabriel_laxy@proton.me') },
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const sair = () => {
+    Alert.alert(
+      'Sair',
+      'Você deseja realmente sair?',
+      [
+        {
+          text: 'Não',
+          onPress: () => console.log('Cancelado'),
+          style: 'cancel',
+        },
+        { text: 'Sim', onPress: () => navigation.navigate('login') },
+      ],
+      { cancelable: false }
+    );
+  };
+
   return (
-    <View style={{ backgroundColor: "#FBFEF4", flex: 1 }}>
+    <View style={styles.container}>
       <Text style={styles.titulo}>
-        {'\n'}Configurações
+        Configurações
       </Text>
-      <View style={{ padding: '5%'}}>
-        <DashedLine dashLength={5} dashThickness={2} dashGap={10} dashColor='#5C4B4B'/>
+      <View style={{ padding: '5%' }}>
+        <DashedLine dashLength={5} dashThickness={2} dashGap={10} dashColor='#5C4B4B' />
       </View>
-      <Text style={styles.topicos}>Sons</Text>
-      <Text style={styles.topicos}>Música</Text>
-      <TouchableOpacity onPress={() => alert('Perfil clicked')}>
+      <Text style={styles.musica}>Música</Text>
+      <Slider
+        style={{ width: '90%', height: 40, alignSelf: 'center' }}
+        minimumValue={0}
+        maximumValue={1}
+        value={volume}
+        minimumTrackTintColor="#26A910"
+        maximumTrackTintColor="#D8D8D8"
+        thumbTintColor='#3FC600'
+      />
+      <TouchableOpacity style={styles.topicosContainers} onPress={() => navigation.navigate('Perfil')}>
         <Text style={styles.topicos}>Perfil</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => alert('Suporte clicked')}>
+      <TouchableOpacity style={styles.topicosContainers} onPress={emailSuporte}>
         <Text style={styles.topicos}>Suporte</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => alert('Sair clicked')}>
+      <TouchableOpacity style={styles.sairContainer} onPress={sair}>
         <Text style={styles.topicos}>Sair</Text>
+        <Entypo name="log-out" size={20} color="#5C4B4B" style={styles.sairIcon} />
       </TouchableOpacity>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FBFEF4',
+  },
   titulo: {
     backgroundColor: "#FBFEF4",
+    alignSelf: 'center',
     color: "#5C4B4B",
     fontFamily: "Poppins_700Bold",
-    fontSize: 27,
-    textAlign: 'center',
-  },
-  linha: {
-    backgroundColor: "#FBFEF4",
-    color: "#5C4B4B",
-    fontFamily: "Poppins_700Bold",
-    fontSize: 20,
-    textAlign: 'center',
+    fontSize: 32,
     marginTop: 20,
-    margin:20,
+  },
+  topicosContainers: {
+    alignSelf: 'flex-start',
+    flexDirection: 'row',
+    paddingLeft: 30,
+    marginTop: 20,
   },
   topicos: {
-    backgroundColor: "#FBFEF4",
     color: "#5C4B4B",
     fontFamily: "Poppins_700Bold",
-    fontSize: 20,
-    textAlign: 'left',
+    fontSize: 25,
+    textDecorationLine: 'underline',
+  },
+  musica: {
+    color: "#5C4B4B",
+    fontFamily: "Poppins_700Bold",
+    fontSize: 25,
     marginTop: 20,
-    marginLeft: 50,
-    textDecorationLine:'underline',
+    textDecorationLine: 'underline',
+    paddingLeft: 30,
+  },
+  sairContainer: {
+    flexDirection: 'row',
+    alignItems: 'center', // Alinha verticalmente o texto e o ícone
+    paddingLeft: 30,
+    marginTop: 20,
+  },
+  sairIcon: {
+    marginLeft: 10, // Espaço entre o texto e o ícone
   },
 });
--- screens/routes.js
+++ screens/routes.js
@@ -1,65 +1,40 @@
 import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TelaLogin from './telaLogin.js';
 import TelaCadastro from './telaCadastro.js';
 import EscolhaNome from './escolhaNome.js';
-import { Config, Exercicios, Home, Tasks, Camera } from '../screens';
-import { NavigationContainer} from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useFonts, Poppins_700Bold, Poppins_500Medium } from '@expo-google-fonts/poppins'
+import { Config, Exercicios, Home, Tasks, Camera, Perfil } from '../screens'; // Ajuste os caminhos conforme necessário
+import { useFonts, Poppins_700Bold, Poppins_500Medium } from '@expo-google-fonts/poppins';
 import { useIsFocused } from '@react-navigation/native';
 import { playBackgroundMusic, stopBackgroundMusic } from '../backend/music.js';
-//import icons
-import { AntDesign } from '@expo/vector-icons';
-//<AntDesign name="home" size={24} color="black" />
-import { Feather } from '@expo/vector-icons';
-//Check 
-{/* <Feather name="check-square" size={24} color="black" /> */}
-//Camera
-//<Feather name="camera" size={24} color="black" />
-import { FontAwesome5 } from '@expo/vector-icons';
-//<FontAwesome5 name="running" size={24} color="black" />
+import { AntDesign, Feather, FontAwesome5 } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
-
 const Stack = createNativeStackNavigator();
 
-
-
 const screenOptions = {
-  tabBarShowLabel:false,
-  headerShown:true,
+  tabBarShowLabel: false,
+  headerShown: true,
   headerTitleAlign: 'center',
   headerTitle: () => (
     <View>
-      <Text style={{ color: '#8AC600', fontSize: 28, fontFamily:'Poppins_700Bold', alignItems:'center',  justifyContent: 'center', marginTop:'3%',}}>FeedIt</Text>
+      <Text style={{ color: '#8AC600', fontSize: 28, fontFamily: 'Poppins_700Bold', alignItems: 'center', justifyContent: 'center', marginTop: '3%' }}>FeedIt</Text>
     </View>
   ),
-  tabBarStyle:{
+  tabBarStyle: {
     position: 'absolute',
     bottom: 0,
     right: 0,
-    left: 0,  
+    left: 0,
     elevation: 0,
     height: 65,
     background: '#5C4B4B'
   }
-}
+};
 
-//color: marrom: #5C4B4B
-//verde: "#8AC600"
-//<Feather name="settings" size={24} color="black" />
-function StackGroup(){
-  return(
-    <Stack.Navigator>
-        <Stack.Screen name="login" options={{ headerShown: false }} component={TelaLogin} />
-        <Stack.Screen name="cadastro" options={{ headerShown: false }} component={TelaCadastro} />
-        <Stack.Screen name="escolhaNome" options={{ headerShown: false }} component={EscolhaNome} />
-        <Stack.Screen name="TabGroup" options={{ headerShown: false }} component={TabGroup}/>
-    </Stack.Navigator>
-  );
-}
 function TabGroup() {
   const isFocused = useIsFocused();
 
@@ -70,125 +45,108 @@ function TabGroup() {
       stopBackgroundMusic();
     }
   }, [isFocused]);
+
   return (
-      <Tab.Navigator screenOptions={screenOptions} initialRouteName= 'Home'>
-        <Tab.Screen name='Tasks' component={Tasks} options={{
-          tabBarIcon: ({ focused }) => (
-            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-              <View style={{
-                width: 45,
-                height: 45,
-                borderRadius: 10,
-                backgroundColor: focused ? '#8AC600' : null,
-                justifyContent: 'center',
-                alignItems: 'center'
-                }}>
-                <Feather name="check-square" size={24} color={focused ? '#ffff' : '#5C4B4B'} />
-              </View>
+    <Tab.Navigator screenOptions={screenOptions} initialRouteName='Home'>
+      <Tab.Screen name='Tasks' component={Tasks} options={{
+        tabBarIcon: ({ focused }) => (
+          <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <View style={[styles.icones, { backgroundColor: focused ? '#8AC600' : null }]}>
+              <Feather name="check-square" size={24} color={focused ? '#ffff' : '#5C4B4B'} />
             </View>
-          )
-        }} />
-        
-        <Tab.Screen name = 'Câmera' 
-        component={Camera}
-        options={{
-          tabBarIcon: ({focused})=>{
-            return (
-              <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-              <View style={{
-                width: 45,
-                height: 45,
-                borderRadius: 10,
-                backgroundColor: focused ? '#8AC600' : null,
-                justifyContent: 'center',
-                alignItems: 'center'
-                }}>
-                <Feather name='camera' size={24} color={focused ? '#ffff' : '#5C4B4B'}background/>
-              </View>
+          </View>
+        )
+      }} />
+      <Tab.Screen name='Câmera' component={Camera} options={{
+        tabBarIcon: ({ focused }) => (
+          <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <View style={[styles.icones, { backgroundColor: focused ? '#8AC600' : null }]}>
+              <Feather name='camera' size={24} color={focused ? '#ffff' : '#5C4B4B'} />
             </View>
-            )
-          }
-        }}
-        />
-        <Tab.Screen name = 'Home' 
-        component={Home}
-        options={{
-          tabBarIcon: ({focused})=>{
-            return (
-              <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-              <View style={{
-                width: 45,
-                height: 45,
-                borderRadius: 10,
-                backgroundColor: focused ? '#8AC600' : null,
-                justifyContent: 'center',
-                alignItems: 'center'
-                }}>
-                <AntDesign name="home" size={30} color={focused ? '#ffff' : '#5C4B4B'}background />
-              </View>
+          </View>
+        )
+      }} />
+      <Tab.Screen name='Home' component={Home} options={{
+        tabBarIcon: ({ focused }) => (
+          <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <View style={[styles.icones, { backgroundColor: focused ? '#8AC600' : null }]}>
+              <AntDesign name="home" size={30} color={focused ? '#ffff' : '#5C4B4B'} />
             </View>
-            )
-          }
-        }}
-        />
-        <Tab.Screen name = 'Exercíco' 
-        component={Exercicios}
-        options={{
-          tabBarIcon: ({focused})=>{
-            return (
-              <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-              <View style={{
-                width: 45,
-                height: 45,
-                borderRadius: 10,
-                backgroundColor: focused ? '#8AC600' : null,
-                justifyContent: 'center',
-                alignItems: 'center'
-                }}>
-                <FontAwesome5 name="running" size={24} color={focused ? '#ffff' : '#5C4B4B'}background />
-              </View>
+          </View>
+        )
+      }} />
+      <Tab.Screen name='Exercício' component={Exercicios} options={{
+        tabBarIcon: ({ focused }) => (
+          <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <View style={[styles.icones, { backgroundColor: focused ? '#8AC600' : null }]}>
+              <FontAwesome5 name="running" size={24} color={focused ? '#ffff' : '#5C4B4B'} />
             </View>
-            )
-          }
-        }}
-        />
-        <Tab.Screen name = 'Configurações' 
-        component={Config}
-        options={{
-          tabBarIcon: ({focused})=>{
-            return (
-              <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-              <View style={{
-                width: 45,
-                height: 45,
-                borderRadius: 10,
-                backgroundColor: focused ? '#8AC600' : null,
-                justifyContent: 'center',
-                alignItems: 'center'
-                }}>
-                <Feather name="settings" size={24} color={focused ? '#ffff' : '#5C4B4B'}background />
-              </View>
+          </View>
+        )
+      }} />
+      <Tab.Screen name='Configurações' component={Config} options={{
+        tabBarIcon: ({ focused }) => (
+          <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <View style={[styles.icones, { backgroundColor: focused ? '#8AC600' : null }]}>
+              <Feather name="settings" size={24} color={focused ? '#ffff' : '#5C4B4B'} />
             </View>
-            )
-          }
-        }}/>
-      </Tab.Navigator>
+          </View>
+        )
+      }} />
+    </Tab.Navigator>
   );
 }
 
-export default function Navigation(){
+function StackGroup() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="login" options={{ headerShown: false }} component={TelaLogin} />
+      <Stack.Screen name="cadastro" options={{ headerShown: false }} component={TelaCadastro} />
+      <Stack.Screen name="escolhaNome" options={{ headerShown: false }} component={EscolhaNome} />
+      <Stack.Screen name="TabGroup" options={{ headerShown: false }} component={TabGroup} />
+      <Stack.Screen name="Perfil" component={Perfil} options={{
+          headerStyle: {
+            backgroundColor: 'white',
+          },
+          headerTintColor: '#5C4B4B',
+          headerTitleAlign: 'center',
+          headerTitle: () => (
+            <View>
+              <Text style={{ color: '#5C4B4B', fontSize: 25, fontFamily: 'Poppins_700Bold', alignItems: 'center', justifyContent: 'center', marginTop: '10%' }}>Perfil</Text>
+            </View>
+          ),
+          headerTitleStyle: {
+            fontFamily: 'Poppins_700Bold',
+            fontSize: 20,
+          },
+        }} />
+    </Stack.Navigator>
+  );
+}
+
+export default function Navigation() {
   const [fontLoaded] = useFonts({
     Poppins_700Bold, Poppins_500Medium,
   });
-  
-  if (!fontLoaded){
+
+  if (!fontLoaded) {
     return null;
   };
-  return(
-      <NavigationContainer>
-          <StackGroup/>
 
-          {/* <TabGroup/> */}
-      </NavigationContainer>
+  return (
+    <NavigationContainer>
+      {/* <TabGroup/> */}
+      <StackGroup />
+    </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  icones: {
+    width: 45,
+    height: 45,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
