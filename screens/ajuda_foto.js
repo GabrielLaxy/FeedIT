03bbfd490000fa1b6198fff733b6a54196bbd8cd
Gabriel Laxy <gablaxycarreira@gmail.com>
2024-05-28 20:59:12 -0300

    criacao da tela ajuda_foto

---
+++ screens/ajuda_foto.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+const foto = require('../assets/foto.png');
+
+export default function TutorialScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tutorial de Fotos Divertidas!</Text>
+      <Text style={styles.instructions}>
+        Olá! Vamos tirar fotos de alimentos deliciosos para ganhar pontos para o seu bichinho!
+      </Text>
+      <Text style={styles.instructions}>
+        1. Centralize o alimento dentro do quadrado na tela.
+      </Text>
+      <Image
+        style={styles.image}
+        source={{ foto }} // Use a URL de uma imagem de exemplo
+      />
+      <Text style={styles.instructions}>
+        2. Tire a foto e veja quantos pontos você ganhou!
+      </Text>
+      <Text style={styles.instructions}>
+        3. Explore e descubra quais alimentos dão mais pontos!
+      </Text>
+      <Text style={styles.instructions}>
+        Divirta-se tirando fotos e cuidando do seu bichinho!
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f8ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 20,
+  },
+  instructions: {
+    fontSize: 18,
+    color: '#555',
+    marginVertical: 10,
+    textAlign: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginVertical: 20,
+    borderWidth: 2,
+    borderColor: '#ddd',
+  },
+});
